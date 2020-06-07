1070132b4cd6f1c7f298b6eff2f511f32d12ec3b
pranavmac <59638773+Pranavmac@users.noreply.github.com>
2020-06-07 10:51:18 +0530

    Add files via upload
    waiting page in application

---
+++ wait.php
@@ -0,0 +1,11 @@
+<section class="listings">
+        <div class="wrapper">
+            <ul class="properties_list">
+                <h2 style="text-align:center; color:#FF0000; font-family: 'Courier New', Courier, monospace">Thank you for sending a request to PRP Car Rentals. 
+                We will get back to you once we verify your payment.<br>
+                You can login to view the processing status of your request using your email and the National ID Number as password.</h2>
+            </ul>
+            
+        </div>
+    </section>
+
