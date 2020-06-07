f1c3e4d40cf3930b3f64b3706540ddfa1e38e757
pranavmac <59638773+Pranavmac@users.noreply.github.com>
2020-06-07 10:54:58 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+    <div class="shell">
+        <span class="left">&copy; <?php echo date("Y");?> - PRP Rescue Team</span>
+        <span class="right">
+            Design by Pranav-Ramachandran-Pradeep</a>
+        </span>
+    </div>
+</div>
+
